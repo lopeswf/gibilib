bcb6657ceafa9de9e367b2105f7d0be772571c3c
Felipe Weber <lopeswf@hotmail.com>
2021-08-22 14:44:50 -0300

    authentication test refactor

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -3,40 +3,68 @@
 namespace Tests\Feature;
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
-    use RefreshDatabase;
+    use DatabaseMigrations;
+
+    const EMAIL_FAIL = 'fail@email';
+    const PASSWORD_FAIL = 'fail';
+    const DEFAULT_PASSWORD = 'password';
+
     /**
      * Registry a new user.
      * 
      * @return void
      */
-    public function test_user_registration_with_authentication()
+    public function test_user_registration()
     {
-        /** @var TestResponse */
-        $authRequest = $this->getJson('/sanctum/csrf-cookie');
-
-        /** @var array */
-        $cookies = $authRequest->headers->getCookies();
-
-        /** @var string */
-        $auth = collect($cookies)->first(fn($cookie) => $cookie->getName() ?? '' === config('sanctum.token_name',''))?->getValue();
-
-        if(empty($auth)){
-            $this->fail('Authentication failed!');
-        }
-
         /** @var array */
         $user = User::factory()->make()->toArray();
-        $user['password_confirmation'] = $user['password'] = '12345678';
+        $user['password_confirmation'] = $user['password'] = Self::DEFAULT_PASSWORD;
 
         /** @var TestResponse */
-        $response = $this->postJson('/register',$user,['X-XSRF-TOKEN' => $auth]);
+        $response = $this->postJson('/register',$user);
 
         $response->assertCreated();
+    }
+
+
+    /**
+     * User login.
+     * 
+     * @return void
+     */
+    public function test_user_login_succeeds()
+    {
+        /** @var User */
+        $user = User::factory()->create();
+
+        /** @var TestResponse */
+        $response = $this->postJson('/login',[
+            'email' => $user->email,
+            'password' => Self::DEFAULT_PASSWORD
+        ]);
+
+        $response->assertOk();
+    }
+
+    /**
+     * User login.
+     * 
+     * @return void
+     */
+    public function test_user_login_fails()
+    {
+        /** @var TestResponse */
+        $response = $this->postJson('/login',[
+            'email' => Self::EMAIL_FAIL,
+            'password' => Self::PASSWORD_FAIL
+        ]);
+
+        $response->assertJsonValidationErrors('email');
     }
 }
